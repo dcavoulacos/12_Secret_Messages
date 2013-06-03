77df75b1d0396d2a3a9db320e349bb4dd4395c90
vagrant <vagrant@precise32.(none)>
2013-06-03 16:20:16 +0000

    added functionality to message breaker

--- Secret_Message.rb
+++ Secret_Message.rb
@@ -1,4 +1,11 @@
-class Secret_Messages
+ 	def Secret_Messages(jumbled_key, jumbled_message)
+ 		key = key_breaker(jumbled_key)
+ 		key_shift = key_converter(jumbled_key)
+ 		puts key_shift
+ 		decoded_message = message_breaker(key_shift, jumbled_message)
+ 		return decoded_message
+ 	end
+
 
 	@dictionary = []
 
@@ -13,17 +20,50 @@ class Secret_Messages
 		return @dictionary.include?()
 	end
 
-	def key_cypher
+	def key_breaker
 
-# cypher is the paragraph(string) that we are decoding
-	cipher_array = cipher.chars
-	cipher_array.each do |char|
-		if /[a-zA-z]/.include?(char)
-			# code to shift
-		else
-			next
+	end
+
+	def key_converter(key)
+		key_shift = []
+		key.upcase.codepoints.each do |letter|
+			key_shift << letter - 65
 		end
+		return key_shift
+	end
+
+	def bytes_to_letters(args)
+		args.pack("c*")
+	end
+
+	def char_shift(begin_char, end_char, current_char, shift)
+		new_char = current_char - shift
+		new_char =+ (end_char - begin_char + 1) if new_char > begin_char
+		return new_char
 	end
 
 
+	def message_breaker(key_shift, message)
+		counter = 0
+		decoded_bytes = []
+		message.each_byte do |byte|
+			counter = counter % key_shift.length
+			shift = key_shift.index(counter)		
+			if (byte >= 65 && byte <= 90)
+				new_letter = char_shift(65, 90, byte, shift)
+				decoded_bytes << new_letter
+				counter =+ 1
+			elsif (byte >= 97 && byte <= 122)
+				new_letter = char_shift(97, 122, byte, shift)
+				decoded_bytes << new_letter
+				counter =+ 1			
+			else
+				decoded_bytes << byte
+			end
+		end
+		decoded_message = bytes_to_letters(decoded_bytes)
+		return decoded_message
+	end
+
+puts bytes_to_letters([65, 97, 90, 122])
 
